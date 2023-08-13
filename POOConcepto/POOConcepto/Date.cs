09ff7b14f9042cfcb4820a8eed86d038aaad5e41
polosn <paul1997.2015@gmail.com>
2023-08-12 23:38:33 -0500

    Finish class.

---
+++ POOConcepto/POOConcepto/Date.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOConcepto
+{
+    public class Date
+    {
+        //atributos
+        private int _year;
+        private int _month;
+        private int _day;
+
+        //metodo constructor y parametros
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = month;
+            _day = day;
+            
+        }
+    }
+}
---
+++ POOConcepto/POOConcepto/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Conceptos de POO");
+
+
+
+Console.ReadKey();
